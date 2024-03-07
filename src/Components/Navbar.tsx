750b129afb0b80a2cca610a3972414ed96b65ee5
arian-sfri <125110161+arian-sfri@users.noreply.github.com>
2024-03-06 18:18:54 -0700

    Basic home page

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,11 @@
+import './Navbar.css';
+function Navbar() {
+    return (
+        <div className="navbar">
+            <a href="#Calendar">Calendar</a>
+            <a href="#Grades">Grades</a>
+        </div>
+    );
+}
+
+export default Navbar;
