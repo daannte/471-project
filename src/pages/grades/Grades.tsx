78ae5ec31791f1b942045872531e7e9302cbf8fc
arian-sfri <125110161+arian-sfri@users.noreply.github.com>
2024-03-24 00:11:50 -0600

    basic grades table

--- src/pages/grades/Grades.tsx
+++ src/pages/grades/Grades.tsx
@@ -1,8 +1,37 @@
+import "./Grades.css";
 import Navbar from "@/components/navbar/Navbar";
 
 function Grades() {
   return (
+    <>
       <Navbar />
+      <div className="grades-container">
+        <div className="row">
+          <div className="long-row">
+            <span>Achieved Grades</span>
+            <span className="grade">F</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="long-row">
+            <span>Tentative Grades</span>
+            <span className="grade">F</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="long-row">
+            <span>Assignments</span>
+            <span className="grade">F</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="long-row">
+            <span>Exams</span>
+            <span className="grade">F</span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
