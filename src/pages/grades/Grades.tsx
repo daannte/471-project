6d6e808f3d252a50b45cb33d88bcf2b466f31060
Dante <dkirsman@gmail.com>
2024-04-01 17:49:13 -0600

    Fix arians 36 errors 🤡

--- src/pages/grades/Grades.tsx
+++ src/pages/grades/Grades.tsx
@@ -2,40 +2,56 @@ import React, { useState } from "react";
 import "./Grades.css";
 import Navbar from "@/components/navbar/Navbar";
 
+interface Component {
+  name: string;
+  points: number | null;
+  weight: number | null;
+  submitted?: boolean;
+}
+
 function Grades() {
-  const [assignments, setAssignments] = useState([]);
-  const [exams, setExams] = useState([]);
+  const [assignments, setAssignments] = useState<Component[]>([]);
+  const [exams, setExams] = useState<Component[]>([]);
 
   // Function to handle adding a new component row
-  const handleAddComponent = (componentType) => {
-    const newComponent = {
+  const handleAddComponent = (componentType: string) => {
+    const newComponent: Component = {
       name: "",
-      points: "",
-      weight: ""
+      points: null,
+      weight: null,
     };
+
     if (componentType === "assignment") {
-      setAssignments([...assignments, newComponent]);
+      setAssignments((prevAssignments) => [...prevAssignments, newComponent]);
     } else if (componentType === "exam") {
-      setExams([...exams, newComponent]);
+      setExams((prevExams) => [...prevExams, newComponent]);
     }
   };
 
   // Function to handle input change for component properties
-  const handleInputChange = (e, index, componentType, property) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number,
+    componentType: string,
+    property: string,
+  ) => {
     const value = e.target.value;
     if (componentType === "assignment") {
       const updatedAssignments = [...assignments];
-      updatedAssignments[index][property] = value;
+      updatedAssignments[index] = {
+        ...updatedAssignments[index],
+        [property]: value,
+      };
       setAssignments(updatedAssignments);
     } else if (componentType === "exam") {
       const updatedExams = [...exams];
-      updatedExams[index][property] = value;
+      updatedExams[index] = { ...updatedExams[index], [property]: value };
       setExams(updatedExams);
     }
   };
 
   // Function to handle submitting component data
-  const handleSubmit = (index, componentType) => {
+  const handleSubmit = (index: number, componentType: string) => {
     if (componentType === "assignment") {
       const updatedAssignments = [...assignments];
       updatedAssignments[index].submitted = true;
@@ -48,7 +64,7 @@ function Grades() {
   };
 
   // Function to handle editing a submitted row
-  const handleEdit = (index, componentType) => {
+  const handleEdit = (index: number, componentType: string) => {
     // Set the 'submitted' property to false to allow editing
     if (componentType === "assignment") {
       const updatedAssignments = [...assignments];
@@ -68,42 +84,66 @@ function Grades() {
         <div className="row">
           <div className="long-row">
             <span>Assignments</span>
-            <button className="add" onClick={() => handleAddComponent("assignment")}>+</button>
+            <button
+              className="add"
+              onClick={() => handleAddComponent("assignment")}
+            >
+              +
+            </button>
             <span className="grade">F</span>
           </div>
         </div>
         {/* Render assignment rows */}
         {assignments.map((assignment, index) => (
-          <div className="row" key={index} style={{ backgroundColor: 'white' }}>
-            <div className={assignment.submitted ? "long-row submitted" : "long-row"} style={{ backgroundColor: 'white' }}>
+          <div className="row" key={index} style={{ backgroundColor: "white" }}>
+            <div
+              className={
+                assignment.submitted ? "long-row submitted" : "long-row"
+              }
+              style={{ backgroundColor: "white" }}
+            >
               {assignment.submitted ? (
                 <>
                   <span>{assignment.name}</span>
                   <span>{assignment.points}</span>
                   <span>{assignment.weight}</span>
-                  <button onClick={() => handleEdit(index, "assignment")}>Edit</button>
+                  <button onClick={() => handleEdit(index, "assignment")}>
+                    Edit
+                  </button>
                 </>
               ) : (
                 <>
                   <input
                     type="text"
                     value={assignment.name}
-                    onChange={(e) => handleInputChange(e, index, "assignment", "name")}
+                    onChange={(e) =>
+                      handleInputChange(e, index, "assignment", "name")
+                    }
                     placeholder="Name"
                   />
                   <input
                     type="text"
-                    value={assignment.points}
-                    onChange={(e) => handleInputChange(e, index, "assignment", "points")}
+                    value={
+                      assignment.points ? assignment.points.toString() : ""
+                    }
+                    onChange={(e) =>
+                      handleInputChange(e, index, "assignment", "points")
+                    }
                     placeholder="Points"
                   />
                   <input
                     type="text"
-                    value={assignment.weight}
-                    onChange={(e) => handleInputChange(e, index, "assignment", "weight")}
+                    value={
+                      assignment.weight ? assignment.weight.toString() : ""
+                    }
+                    onChange={(e) =>
+                      handleInputChange(e, index, "assignment", "weight")
+                    }
                     placeholder="Weight"
                   />
-                  <button onClick={() => handleSubmit(index, "assignment")}>Submit</button>
+                  <button onClick={() => handleSubmit(index, "assignment")}>
+                    Submit
+                  </button>
                 </>
               )}
             </div>
@@ -112,42 +152,57 @@ function Grades() {
         <div className="row">
           <div className="long-row">
             <span>Exams</span>
-            <button className="add" onClick={() => handleAddComponent("exam")}>+</button>
+            <button className="add" onClick={() => handleAddComponent("exam")}>
+              +
+            </button>
             <span className="grade">F</span>
           </div>
         </div>
         {/* Render exam rows */}
         {exams.map((exam, index) => (
-          <div className="row" key={index} style={{ backgroundColor: 'white' }}>
-            <div className={exam.submitted ? "long-row submitted" : "long-row"} style={{ backgroundColor: 'white' }}>
+          <div className="row" key={index} style={{ backgroundColor: "white" }}>
+            <div
+              className={exam.submitted ? "long-row submitted" : "long-row"}
+              style={{ backgroundColor: "white" }}
+            >
               {exam.submitted ? (
                 <>
                   <span>{exam.name}</span>
                   <span>{exam.points}</span>
                   <span>{exam.weight}</span>
-                  <button onClick={() => handleEdit(index, "exam")}>Edit</button>
+                  <button onClick={() => handleEdit(index, "exam")}>
+                    Edit
+                  </button>
                 </>
               ) : (
                 <>
                   <input
                     type="text"
                     value={exam.name}
-                    onChange={(e) => handleInputChange(e, index, "exam", "name")}
+                    onChange={(e) =>
+                      handleInputChange(e, index, "exam", "name")
+                    }
                     placeholder="Name"
                   />
                   <input
                     type="text"
-                    value={exam.points}
-                    onChange={(e) => handleInputChange(e, index, "exam", "points")}
+                    value={exam.points ? exam.points.toString() : ""}
+                    onChange={(e) =>
+                      handleInputChange(e, index, "exam", "points")
+                    }
                     placeholder="Points"
                   />
                   <input
                     type="text"
-                    value={exam.weight}
-                    onChange={(e) => handleInputChange(e, index, "exam", "weight")}
+                    value={exam.weight ? exam.weight.toString() : ""}
+                    onChange={(e) =>
+                      handleInputChange(e, index, "exam", "weight")
+                    }
                     placeholder="Weight"
                   />
-                  <button onClick={() => handleSubmit(index, "exam")}>Submit</button>
+                  <button onClick={() => handleSubmit(index, "exam")}>
+                    Submit
+                  </button>
                 </>
               )}
             </div>
