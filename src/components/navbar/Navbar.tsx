beaae7a229e47b555594d269abb3814f4e0fecbf
Dante <dkirsman@gmail.com>
2024-04-01 19:26:10 -0600

    Added logout feature and change what can be seen while not logged in

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
+import isAuthenticated from "@/utils/auth";
 import "./Navbar.css";
+
 function Navbar() {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/calendar">Calendar</Link>
-      <Link className="navbar__login-text" to="/login">
-        Login
-      </Link>
-      <Link to = "/Grades">Grades</Link>
+      {isAuthenticated() ? (
+        <>
+          <Link to="/">Home</Link>
+          <Link to="/calendar">Calendar</Link>
+          <Link to="/Grades">Grades</Link>
+          <Link to="/logout">Logout</Link>
+        </>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
     </nav>
   );
 }
