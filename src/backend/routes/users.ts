d74a443b199d7487644b0de8d23d37e5d356fe50
zoekirsman <zoe.kirsman@ucalgary.ca>
2024-04-04 19:58:00 -0600

    added users.ts query

--- src/backend/routes/users.ts
+++ src/backend/routes/users.ts
@@ -4,16 +4,13 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   const { cname, cnum, ucid } = req.query;
-  console.log(cname);
-  console.log(cnum);
-  console.log(ucid);
 
   let query;
   let queryParams: any[];
 
-  if (ucid) {
-    query = "SELECT role_type FROM user WHERE ucid = ?;";
-    queryParams = [ucid];
+  if (cname && cnum && ucid) {
+    query = "SELECT * FROM section WHERE course_name=? AND course_num=? AND (ta_id=? OR instr_id=?);";
+    queryParams = [cname, cnum, ucid, ucid];
   } else {
     query = "SELECT * FROM user;";
     queryParams = [];
